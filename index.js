562f33920ff34c0a9552140cc269259a9658c238
msamgan <mohdsamgankhan@gmail.com>
2020-11-30 17:39:34 +0400

    updated the system with env

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+console.log(`Node environment: ${process.env.NODE_ENV}`);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
